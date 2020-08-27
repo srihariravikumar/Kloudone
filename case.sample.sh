cda21bdcd1d805694438edb9e1734f5e4b955eec
Srihari <tizzysrihari@gmail.com>
2020-08-27 15:42:53 +0530

    Update case.sample.sh

--- case.sample.sh
+++ case.sample.sh
@@ -10,3 +10,11 @@ echo "You got 3rd prize" ;;
 0)
 echo "Sorry, try for the next time" ;;
 esac
+****************************************
+****************************************
+****************************************
+O/P
+srihari@srihari:~$ bash case.sample.sh
+Enter your lucky number
+9
+You got 3rd prize
