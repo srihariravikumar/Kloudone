3eddd9a97ef3ea8ac0e225f5ab8c92bbca27305f
Srihari <tizzysrihari@gmail.com>
2020-08-27 15:45:16 +0530

    Update func_parameter.sample.sh

--- func_parameter.sample.sh
+++ func_parameter.sample.sh
@@ -3,3 +3,9 @@ area=$(($1 * $2))
 echo "Area is : $area"
 }
 Rectangle_Area 10 20
+***********************
+***********************
+***********************
+O/P
+srihari@srihari:~$ bash func_parameter.sample.sh
+Area is : 200
