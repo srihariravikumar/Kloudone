f64d82a1eee77a9b4457e1230e3fcf19d83d039c
Srihari <tizzysrihari@gmail.com>
2020-08-27 15:46:29 +0530

    Update pass_return_val_frm_func.sample.sh

--- pass_return_val_frm_func.sample.sh
+++ pass_return_val_frm_func.sample.sh
@@ -1,12 +1,16 @@
 function hello() {
-
 str="Hi, $name"
 echo $str
-
 }
-
 echo "Enter your name"
 read name
-
 val=$(hello)
 echo "Return value of the function is $val"
+********************************************
+********************************************
+********************************************
+O/P
+srihari@srihari:~$ bash pass_return_val_frm_func.sample.sh
+Enter your name
+Srihari
+Return value of the function is Hi, Srihari
